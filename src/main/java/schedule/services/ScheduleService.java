7c954d484e7454408c6714684a883a9d270e4d0a
Giovanni <giovannidossb@gmail.com>
2023-05-23 21:59:13 -0300

    halting work on schedule as it's not necessary for alpha release

--- src/main/java/schedule/services/ScheduleService.java
+++ src/main/java/schedule/services/ScheduleService.java
@@ -1,5 +1,12 @@
 package schedule.services;
 
+import schedule.entities.ScheduleEntity;
+import schedule.repositories.ScheduleRepository;
+import lombok.AllArgsConstructor;
+
+import java.sql.SQLException;
+
+@AllArgsConstructor
 public class ScheduleService {
     
 }
