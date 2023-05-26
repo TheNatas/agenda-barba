207931ce2589679572699cb84ee268ba05faaa9f
Giovanni <giovannidossb@gmail.com>
2023-05-25 22:11:30 -0300

    fixed import of delete controller

--- src/main/java/workInterval/controllers/DeleteWorkIntervalsController.java
+++ src/main/java/workInterval/controllers/DeleteWorkIntervalsController.java
@@ -1,8 +1,8 @@
 package workInterval.controllers;
 
-import workInterval.dtos.FullWorkIntervalDto;
+import workInterval.dtos.MinWorkIntervalDto;
 import workInterval.factories.WorkIntervalFactory;
-import workInterval.services.UpdateWorkIntervalsService;
+import workInterval.services.DeleteWorkIntervalsService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import utils.Connector;
@@ -11,7 +11,7 @@ import java.net.URI;
 import java.sql.Connection;
 
 public class DeleteWorkIntervalsController {
-    public static ResponseEntity<Integer> execute(FullWorkIntervalDto workIntervalDto) {
+    public static ResponseEntity<Integer> execute(MinWorkIntervalDto workIntervalDto) {
         try {
             Connector connector = new Connector();
             Connection conn = connector.getConnection();
