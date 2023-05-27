9ad6f1e237fbf9ca00fe4e2ba2aea857b7182709
Natanael Alves Gabriel <natasnael2002@gmail.com>
2023-05-27 14:42:06 -0300

    change barber shop get endpoint

--- src/main/java/barberShop/routes/BarberShopRoutes.java
+++ src/main/java/barberShop/routes/BarberShopRoutes.java
@@ -3,17 +3,14 @@ package barberShop.routes;
 import barberShop.controllers.GetBarberShopInfoController;
 import barberShop.entities.BarberShopEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class BarberShopRoutes {
 
-    @GetMapping("/barberShop")
-    public ResponseEntity<BarberShopEntity> getBarberShopInfo(@RequestParam(value = "id", defaultValue = "-1") Integer id) {
+    @GetMapping("/barberShop/{id}")
+    public ResponseEntity<BarberShopEntity> getBarberShopInfo(@PathVariable Integer id) {
         return GetBarberShopInfoController.execute(id);
     }
 
