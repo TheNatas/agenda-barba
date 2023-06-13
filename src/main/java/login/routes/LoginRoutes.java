dd839b080303d628fdacaa1d1a6d1d2dd66aeb83
Alisson Zanoni <alisson.zanonii@gmail.com>
2023-06-13 14:04:04 -0300

    Alterando o metodo ResponseEntity do login

--- src/main/java/login/routes/LoginRoutes.java
+++ src/main/java/login/routes/LoginRoutes.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class LoginRoutes {
 
     @PostMapping("/login")
-    public ResponseEntity<LoggedUserEntity> login(@RequestBody LoginDto loginDto) {
+    public ResponseEntity<?> login(@RequestBody LoginDto loginDto) {
         return LoginController.execute(loginDto);
     }
 
